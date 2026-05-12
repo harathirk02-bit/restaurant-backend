bcb70fb5318e4374cc3169b3519ea441f0cd719f
Deekshitha <deekshitha@gmail.com>
2026-05-12 14:45:57 +0530

    solved

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ app = FastAPI(
 from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://restaurant-frontend-pgnu.vercel.app"],
     allow_credentials=True,
-    allow_methods=["https://restaurant-frontend-pgnu.vercel.app"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
